c0a94512b9f2af2ab6ff474135d7b13462864b97
Christoffer Bengts Laptop <christoffer.bengts@gmail.com>
2018-12-23 23:37:40 +0100

    Added input/output panels and functionality

--- main.js
+++ main.js
@@ -1,4 +1,11 @@
 
+function updateOutput(params) {
+
+    $("iframe").contents().find("html").html("<html><head><style type='text/css'>" + $("#cssPanel").val() + "</style></head><body>" + $("#htmlPanel").val() + "</body></html>");
+
+    document.getElementById("outputPanel").contentWindow.eval($("#javascriptPanel").val());
+    
+}
 
 $(".toggleButton").hover(function () {
 
@@ -15,5 +22,25 @@ $(".toggleButton").click(function () {
     $(this).toggleClass("active");
 
     $(this).removeClass("highLightedButton");
+
+    var panelId = $(this).attr("id") + "Panel";
+
+    $("#" + panelId).toggleClass("hidden");
+
+    var numberOfActivePanels = 4 - $(".hidden").length;
+
+    $(".panel").width(($(window).width() / numberOfActivePanels) - 5);
+
+});
+
+$(".panel").height($(window).height() - $("#header").height() - 15);
+
+$(".panel").width(($(window).width() / 2) - 5);
+
+updateOutput();
+
+$("textarea").on("change keyup paste", function (params) {
+    
+    updateOutput();
 
 });
